4cceb7163cbcdee1a08ade5af045df95c8022a83
Christopher Cahill <cthomascahill@gmail.com>
2019-01-18 13:37:48 -0800

    first and last name lookups wiring up UI and API

--- src/doctor.js
+++ src/doctor.js
@@ -1,14 +1,27 @@
 export class Doctor {
-  constructor(name) {
-  this.name = name;
-
-
+  constructor() {
   }
 
   getDoctor(issue, lat, long) {
     return new Promise(function(resolve, reject) {
     let request = new XMLHttpRequest();
     let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${issue}&location=${lat}%2C${long}%2C50&user_location=${lat}%2C${long}&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+
+  getDoctorNames(firstName, lastName, lat, long) {
+    return new Promise(function(resolve, reject) {
+    let request = new XMLHttpRequest();
+    let url = `https://api.betterdoctor.com/2016-03-01/doctors?first_name=${firstName}&last_name=${lastName}&location=${lat}%2C${long}%2C50&user_location=${lat}%2C${long}&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
--- src/main.js
+++ src/main.js
@@ -25,12 +25,71 @@ $(document).ready(function() {
     APICallIssue(userIssue,lookupZip.latitude,lookupZip.longitude);
   });
 
-  $("nameForm").submit(function(event) {
+  $("#nameForm").submit(function(event) {
     event.preventDefault();
+    const userFirstName = $("#inputDrFirst").val();
+    const userLastName = $("#inputDrLast").val();
+    const userZipNames = $("#inputCity2").val();
+    const lookupZipNames = zipcodes.lookup(userZipNames);
+    console.log(userIssue);
+    console.log(userZip);
+    console.log(lookupZip.city, lookupZip.latitude, lookupZip.longitude);
+    APICallNames(userIssue,lookupZip.latitude,lookupZip.longitude);
   });
 
   function APICallIssue(issue,lat,long) {
     let promise = doctor.getDoctor(issue, lat, long);
+    promise.then(function(response) {
+      let body = JSON.parse(response);
+      console.log(body);
+      if (body.data.length == 0) {
+        $(".resultDiv").text("no doctors fit that criteria")
+      }
+      else {
+        body.data.forEach(function(element) {
+          let acceptingValue = element.practices[0].accepts_new_patients
+          function returnYesNo(value) {
+            let result;
+            if (value == true) {
+              result = "Yes"
+            }
+            else {result = "No"}
+            return result
+          }
+          const acceptNewPatients = returnYesNo(acceptingValue);
+          $(".resultDiv").append(`<div class="accordion" id="accordionExample">
+                                    <div class="card">
+                                      <div class="card-header" id="headingOne">
+                                        <h5 class="mb-0">
+                                          <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                                            ${element.profile.last_name}, ${element.profile.first_name}, ${element.profile.title}
+                                          </button>
+                                        </h5>
+                                      </div>
+
+                                      <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+                                        <div class="card-body">
+                                          <img src="${element.profile.image_url}">
+                                          <br>
+                                          ${element.profile.bio}
+                                          <br>
+                                          Phone Number - ${element.practices[0].phones[0].number}
+                                          <br>
+                                          Address - ${element.practices[0].name} ${element.practices[0].visit_address.city}, ${element.practices[0].visit_address.state_long} ${element.practices[0].visit_address.street} ${element.practices[0].visit_address.zip}
+                                          <br>
+                                          Accepting New Patients - ${acceptNewPatients}
+                                        </div>
+                                      </div>`
+            )
+        })
+      }
+    },
+    function(error) {
+      $('.resultDiv').text(`There was an error processing your request`);
+    });
+  }
+  function APICallNames(firstName,lastName,lat,long) {
+    let promise = doctor.getDoctorNames(firstName, lastName lat, long);
     promise.then(function(response) {
       let body = JSON.parse(response);
       console.log(body);
